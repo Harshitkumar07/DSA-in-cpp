dc2bef8c6ffcbd79869f7e78e41f6220fefc6c77
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-09-29 19:23:55 +0530

    Create Factorial of number.cpp

---
+++ Recursion/Factorial of number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n) {
+    if(n == 0 || n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Factorial: " << factorial(num) << endl;
+    return 0;
+}
