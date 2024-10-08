c4fac8464038bc356b229e353045be2fab810cd8
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-08 06:38:25 +0530

    Update Check if number is a prime.cpp

--- Recursion/Check if number is a prime.cpp
+++ Recursion/Check if number is a prime.cpp
@@ -1,4 +1,5 @@
-/*Problem Statement:
+/*
+Problem Statement:
 Check if a number is prime using recursion.
 
 Approach:
@@ -8,7 +9,8 @@ Approach:
 ◾ Recursive Case:
   ◾ Check divisibility by all numbers from 2 to sqrt(n).
 
-Code : */
+Code :
+*/
 
 #include<iostream>
 using namespace std;
@@ -34,14 +36,12 @@ int main() {
     return 0;
 }
 
-/*◾ Time Complexity: O(sqrt(n))
-
+/*
+◾ Time Complexity: O(sqrt(n))
 The function checks divisibility only up to the square root of n, leading to a time complexity of O(sqrt(n)).
 
 ◾ Space Complexity: O(sqrt(n))
-
 The recursion depth is proportional to the square root of n because it checks divisibility incrementally up to that point.
-
 
 Example Inputs and Outputs:
 
@@ -49,4 +49,5 @@ Example Inputs and Outputs:
    ◾ Output: 7 is prime.
 
 2. ◾ Input: Enetr a number :  10
-   ◾ Output: 10 is not prime.*/
+   ◾ Output: 10 is not prime.
+*/
