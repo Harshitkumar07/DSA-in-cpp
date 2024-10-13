76c2643441a9d8dd84942a4976c71891ce304096
Harshit Kumar <kumarharshit9911@gmail.com>
2024-10-13 16:25:57 +0530

    Update Find Greater Common Divisor (GCD).cpp

--- Recursion/Find Greater Common Divisor (GCD).cpp
+++ Recursion/Find Greater Common Divisor (GCD).cpp
@@ -19,17 +19,20 @@ Code -
 #include<iostream>
 using namespace std;
 
-int gcd(int a, int b) {
-    if (b == 0)
+int gcd (int a, int b)
+{
+    if (b == 0) {
         return a;
-    return gcd(b, a % b);
+    }
+    return gcd (b, a % b);
 }
 
-int main() {
+int main()
+{
     int a, b;
     cout << "Enter two numbers: ";
     cin >> a >> b;
-    cout << "GCD of " << a << " and " << b << ": " << gcd(a, b) << endl;
+    cout << "GCD of " << a << " and " << b << ": " << gcd (a, b) << endl;
     return 0;
 }
 
