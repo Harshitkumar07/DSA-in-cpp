868b07a441fbeaa9c16cab95ab0d231156e012f5
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-20 16:09:38 +0530

    Update 169. Majority element.cpp

--- Arrays/169. Majority element.cpp
+++ Arrays/169. Majority element.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // Moore's voting algoritm
+        int freq = 0; //Frequency of element in array
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (freq == 0) {
+                ans = nums[i];
+            }
+            if (ans == nums[i]) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
