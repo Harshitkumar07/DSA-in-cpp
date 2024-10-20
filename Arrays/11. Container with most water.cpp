d08c67f784c021e326a56478a55467556d26c144
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-20 16:06:47 +0530

    Update 11. Container with most water.cpp

--- Arrays/11. Container with most water.cpp
+++ Arrays/11. Container with most water.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        //Two pointer solutions
+        int lp = 0; //left pointer
+        int rp = height.size() - 1; //right pointer
+        int maxWater = 0;
+        while (lp < rp) {
+            int water = rp - lp;
+            int ht = min(height[lp], height[rp]);
+            int currWater = water * ht;
+            maxWater = max(maxWater, currWater);
+            //Ternary operator
+            height[lp] < height[rp] ? lp++ : rp--;
+        }
+        return maxWater;
+    }
+};
