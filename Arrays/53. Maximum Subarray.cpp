02b6656a13d4a1ce927f6c3c81452e403679d6c8
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-20 16:08:18 +0530

    Update 53. Maximum Subarray.cpp

--- Arrays/53. Maximum Subarray.cpp
+++ Arrays/53. Maximum Subarray.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        //Kadane's Algoritm
+        int currSum = 0; //current sum
+        int maxSum = INT_MIN; //maximum sum
+        for (int n : nums) {
+            currSum = currSum + n;
+            maxSum = max(maxSum, currSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
