a4375266b75f2b751472f2b787655d2479bde7f2
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-20 16:11:18 +0530

    Update 238. Product of array Except Self.cpp

--- Arrays/238. Product of array Except Self.cpp
+++ Arrays/238. Product of array Except Self.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        // Multiply prefix and suffix except that index
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        //prefix >= ans , prefix in stored directly in ans
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+        int suffix = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            suffix *= nums[i + 1];
+            ans[i] *= suffix;
+        }
+        return ans;
+    }
+};
