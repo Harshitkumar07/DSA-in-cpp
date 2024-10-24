ed164c8891077d5b6d32476d2a9c3efe50fe9a6b
Harshit Kumar <149350677+Harshitkumar07@users.noreply.github.com>
2024-10-24 19:07:16 +0530

    Update 14. Longest Common Prefix.cpp

--- Arrays/14. Longest Common Prefix.cpp
+++ Arrays/14. Longest Common Prefix.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string s = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            int k = 0, j = 0;
+            while (k < s.size() || j < strs[i].size()) {
+                if (s[k] == strs[i][j]) {
+                    k++;
+                    j++;
+                } else {
+                    s = strs[i].substr(0, j);
+                    break;
+                }
+            }
+        }
+        return s;
+    }
+};
